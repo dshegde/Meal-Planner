d66368bea1028d163dcb2dc4cfe5a752e6438dda
Deepa Hegde <hegde@pdx.edu>
2023-03-01 19:28:53 -0800

    Added user&IP migrations

--- backend/src/db/datasources/dev_datasource.ts
+++ backend/src/db/datasources/dev_datasource.ts
@@ -4,7 +4,7 @@ import { DataSource } from 'typeorm';
 // Similar reasoning as above, we need to add the file extensions to this file's imports for CLI usage
 import { User } from "../models/user";
 import { IPHistory } from "../models/ip_history";
-import { Initialize1676281754950 } from "../migrations/1676281754950-Initialize";
+import { InitialUserAndIP1677727436444 } from "../migrations/1677727436444-InitialUserAndIP.js";
 
 dotenv.config();
 
@@ -12,21 +12,20 @@ dotenv.config();
 const env = process.env;
 
 export const AppDataSource = new DataSource({
-	type: "postgres",
-	host: env.VITE_DB_HOST,
-	port: Number(env.VITE_DB_PORT),
-	username: env.VITE_DB_USER,
-	password: env.VITE_DB_PASS,
-	database: env.VITE_DB_NAME,
-	// entities are used to tell TypeORM which tables to create in the database
-	entities: [
-		User,
-		IPHistory
-	],
-	migrations: [
-		Initialize1676281754950,
-
-	],
-	// DANGER DANGER our convenience will nuke production data!
-	synchronize: false
+    type: "postgres",
+    host: env.VITE_DB_HOST,
+    port: Number(env.VITE_DB_PORT),
+    username: env.VITE_DB_USER,
+    password: env.VITE_DB_PASS,
+    database: env.VITE_DB_NAME,
+    // entities are used to tell TypeORM which tables to create in the database
+    entities: [
+        User,
+        IPHistory
+    ],
+    migrations: [
+        InitialUserAndIP1677727436444
+    ],
+    // DANGER DANGER our convenience will nuke production data!
+    synchronize: false
 });
