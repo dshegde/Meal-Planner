72dbbe12c3b716b9df9fce8c4a85a4a468ef5949
SayleeMJ <sjagtap@pdx.edu>
2023-11-19 16:20:35 -0800

    Refactor NavMain.tsx for Improved Readability and added separate file for paths

---
+++ frontend/src/components/ConstantsPaths.tsx
@@ -0,0 +1,13 @@
+export const URLS = {
+  HOME: "/",
+  USERS: "/users",
+  RECIPES: "/recipes",
+  SHOPPING_LIST: "/shoppinglist",
+  ALL_MEAL_PLANS: "/mealplan/all",
+  DAY_MEAL_PLANS: "/mealplan/day",
+  DELETE_MEAL_PLAN: "/mealplan/delete",
+  POST_MEAL_PLAN: "/mealplan/post",
+  LOGIN: "http://localhost:3000/login",
+  LOGOUT: "http://localhost:3000/logout",
+  MEAL_PLANS: "/mealplan",
+};
--- frontend/src/components/NavMain.tsx
+++ frontend/src/components/NavMain.tsx
@@ -1,10 +1,8 @@
 import { Link, Route, Routes } from "react-router-dom";
-import Home from "./Home";
 import { MealPlanForm } from "./DeleteMealPlan";
 import { Recipes } from "./Recipe";
 import { Users } from "./User";
 import Button from "react-bootstrap/Button";
-import { useEffect, useState } from "react";
 import "../Dashboard.css";
 import { MealPlanForUser } from "./MealPlanAllUsers";
 import { MealPlanForDay } from "./MealPlanDay";
@@ -12,7 +10,7 @@ import { ShoppingList } from "./ShoppingList";
 import { PostMealPlanForm } from "./PostMealPlan";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import Cookies from "js-Cookie";
-
+import { URLS } from "./ConstantsPaths";
 
 export function NavMain() {
   return (
@@ -24,111 +22,89 @@ export function NavMain() {
 }
 
 function PublicLinksView() {
-	if (Cookies.get("user_id")) {
-		return (<Navbar fixed="top" bg="light" expand="lg">
-		<Container>
-		  <Navbar.Brand>
-			<Link to="/">Home</Link>
-		  </Navbar.Brand>
-		  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-		  <Navbar.Collapse id="basic-navbar-nav">
-			<Nav className="me-auto">
-			  <Nav.Link>
-				<Link to="/users">Users</Link>
-			  </Nav.Link>
-			  <Nav.Link>
-				<Link to="/recipes">Recipes</Link>
-			  </Nav.Link>
-			  <Nav.Link>
-				<Link to="/shoppinglist">Shopping List</Link>
-			  </Nav.Link>
-			  <NavDropdown title="MealPlans" id="basic-nav-dropdown">
-				<NavDropdown.Item>
-				  <Link to="/mealplan/all">List all MealPlan</Link>
-				</NavDropdown.Item>
-				<NavDropdown.Item>
-				  <Link to="/mealplan/day">List MealPlan by day</Link>
-				</NavDropdown.Item>
-				<NavDropdown.Item>
-				  <Link to="/mealplan/delete">Delete Meal Plan</Link>
-				</NavDropdown.Item>
-				<NavDropdown.Item>
-				  <Link to="/mealplan/post">Add Meal Plan</Link>
-				</NavDropdown.Item>
-			  </NavDropdown>
-			</Nav>
-			<Nav>
-			  <Button variant="outline-primary" onClick={LogOutFunc}>
-				Logout
-			  </Button>
-			</Nav>
-		  </Navbar.Collapse>
-		</Container>
-	  </Navbar>);
-	}
-	else {
-		return (<Navbar fixed="top" bg="light" expand="lg">
-		<Container>
-		  <Navbar.Brand>
-			<Link to="/">Home</Link>
-		  </Navbar.Brand>
-		  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-		  <Navbar.Collapse id="basic-navbar-nav">
-			<Nav className="me-auto">
-			  <Nav.Link>
-				<Link to="/users">Users</Link>
-			  </Nav.Link>
-			  <Nav.Link>
-				<Link to="/recipes">Recipes</Link>
-			  </Nav.Link>
-			  <Nav.Link>
-				<Link to="/shoppinglist">Shopping List</Link>
-			  </Nav.Link>
-			  <NavDropdown title="MealPlans" id="basic-nav-dropdown">
-				<NavDropdown.Item>
-				  <Link to="/mealplan/all">List all MealPlan</Link>
-				</NavDropdown.Item>
-				<NavDropdown.Item>
-				  <Link to="/mealplan/day">List MealPlan by day</Link>
-				</NavDropdown.Item>
-				<NavDropdown.Item>
-				  <Link to="/mealplan/delete">Delete Meal Plan</Link>
-				</NavDropdown.Item>
-				<NavDropdown.Item>
-				  <Link to="/mealplan/post">Add Meal Plan</Link>
-				</NavDropdown.Item>
-			  </NavDropdown>
-			</Nav>
-			<Nav>
-			  <Button variant="outline-primary" onClick={LoginFunc}>
-				Login
-			  </Button>
-			</Nav>
-		  </Navbar.Collapse>
-		</Container>
-	  </Navbar>);
-	}
-
+  const isUserLoggedIn = Cookies.get("user_id");
+  return (
+    <Navbar fixed="top" bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          <Link to={URLS.HOME}>Home</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">{getNavLinks()}</Nav>
+          <Nav>
+            {isUserLoggedIn ? (
+              <Button variant="outline-primary" onClick={LogOutFunc}>
+                Logout
+              </Button>
+            ) : (
+              <Button variant="outline-primary" onClick={LoginFunc}>
+                Login
+              </Button>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 function NavRoutes() {
   return (
     <Routes>
-      <Route path="/users" element={<Users />} />
-      <Route path="/recipes" element={<Recipes />} />
-      <Route path="/mealplan/all" element={<MealPlanForUser />} />
-      <Route path="/mealplan/day" element={<MealPlanForDay />} />
-      <Route path="/mealplan/delete" element={<MealPlanForm />} />
-      <Route path="/mealplan/post" element={<PostMealPlanForm />} />
-      <Route path="/shoppinglist" element={<ShoppingList />} />
+      <Route path={URLS.USERS} element={<Users />} />
+      <Route path={URLS.RECIPES} element={<Recipes />} />
+      <Route path={URLS.ALL_MEAL_PLANS} element={<MealPlanForUser />} />
+      <Route path={URLS.DAY_MEAL_PLANS} element={<MealPlanForDay />} />
+      <Route path={URLS.DELETE_MEAL_PLAN} element={<MealPlanForm />} />
+      <Route path={URLS.POST_MEAL_PLAN} element={<PostMealPlanForm />} />
+      <Route path={URLS.SHOPPING_LIST} element={<ShoppingList />} />
     </Routes>
   );
 }
 
 function LoginFunc() {
-  window.location.href = "http://localhost:3000/login";
+  window.location.href = URLS.LOGIN;
 }
 
 function LogOutFunc() {
-  window.location.href = "http://localhost:3000/logout";
+  window.location.href = URLS.LOGOUT;
+}
+
+function getNavLinks() {
+  return (
+    <>
+      <Nav.Link>
+        <Link to={URLS.USERS}>Users</Link>
+      </Nav.Link>
+      <Nav.Link>
+        <Link to={URLS.RECIPES}>Recipes</Link>
+      </Nav.Link>
+      <Nav.Link>
+        <Link to={URLS.SHOPPING_LIST}>Shopping List</Link>
+      </Nav.Link>
+      <NavDropdown title="MealPlans" id="basic-nav-dropdown">
+        {getDropdownMealPlanLinks()}
+      </NavDropdown>
+    </>
+  );
+}
+
+function getDropdownMealPlanLinks() {
+  return (
+    <>
+      <NavDropdown.Item>
+        <Link to={URLS.ALL_MEAL_PLANS}>List all MealPlan</Link>
+      </NavDropdown.Item>
+      <NavDropdown.Item>
+        <Link to={URLS.DAY_MEAL_PLANS}>List MealPlan by day</Link>
+      </NavDropdown.Item>
+      <NavDropdown.Item>
+        <Link to={URLS.DELETE_MEAL_PLAN}>Delete Meal Plan</Link>
+      </NavDropdown.Item>
+      <NavDropdown.Item>
+        <Link to={URLS.POST_MEAL_PLAN}>Add Meal Plan</Link>
+      </NavDropdown.Item>
+    </>
+  );
 }
